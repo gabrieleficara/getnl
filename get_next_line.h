5a66ae292f7b05d9d9f1d894eb914f95c656770d
Gabriele FICARA <gficara@e1r2p12.42.fr>
2017-12-15 19:10:28 +0100

    header

---
+++ get_next_line.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gficara <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/12/15 16:52:43 by gficara           #+#    #+#             */
+/*   Updated: 2017/12/15 18:44:02 by gficara          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <sys/types.h>
+# include <sys/uio.h>
+# include <unistd.h>
+# include "libft/libft.h"
+
+# define BUFF_SIZE 3
+
+int		get_next_line(const int fd, char **line);
+
+#endif
