fb3bd75ebb43ddead7ea0980e99df5901d6af1f9
Gabriele FICARA <gficara@e1r2p12.42.fr>
2017-12-15 19:56:18 +0100

    mi odio

--- get_next_line.c
+++ get_next_line.c
@@ -1,9 +1,16 @@
-#include <sys/types.h>
-#include <sys/uio.h>
-#include <unistd.h>
-#include "libft/libft.h"
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gficara <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/12/15 16:49:15 by gficara           #+#    #+#             */
+/*   Updated: 2017/12/15 19:53:03 by gficara          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-#define BUFFER_SIZE 30
+#include "get_next_line.h"
 
 static int	ft_srchln(int j, char **str, char **line, int use)
 {
@@ -17,18 +24,32 @@ static int	ft_srchln(int j, char **str, char **line, int use)
 	{
 		*line = ft_strnew(j);
 		*line = ft_strncpy(*line, new, j);
-		if (new[j] == '\n')
+		if (new[j] == '\n' || new[j] == '\0')
 		{
-			j++;
-			if (ft_strlen(new) - j != 0)
+			j = (new[j] == '\n') ? j + 1 : j;
+			if (new[j])
 				new = ft_strdup(new + j);
 			else
-				ft_strdel(str);
+				ft_strdel(&new);
 			new = (new) ? new : NULL;
 		}
 	}
 	*str = new;
 	return (j);
+}
+
+static void	init(int *i, char **str, char **line)
+{
+	i[0] = 1;
+	i[1] = 0;
+	i[2] = 1;
+	if (*str)
+	{
+		i[1] = ft_srchln(i[1], str, line, 0);
+		i[2] = ((*str)[i[1]] == '\n') ? 0 : 1;
+	}
+	else
+		*str = ft_strnew(0);
 }
 
 /*
@@ -37,30 +58,20 @@ static int	ft_srchln(int j, char **str, char **line, int use)
 
 int			get_next_line(const int fd, char **line)
 {
-	char	buf[BUFFER_SIZE + 1];
+	char		buf[BUFF_SIZE + 1];
 	static char	*str;
-	int		i[3];
+	int			i[3];
 
-	i[0] = 1;
-	i[1] = 0;
-	i[2] = 1;
-	if (str)
-	{
-		i[1] = ft_srchln(i[1], &str, line, 0);
-		i[2] = (str[i[1]] == '\n') ? 0 : 1;
-	}
-	else
-		str = ft_strnew(0);
+	init(i, &str, line);
 	while (i[0] && i[2] == 1)
 	{
-		i[0] = read(fd, buf, BUFFER_SIZE);
+		i[0] = read(fd, buf, BUFF_SIZE);
 		if (i[0] == -1)
 			return (-1);
 		buf[i[0]] = '\0';
 		str = ft_sfstrjoin(str, buf, 1);
-		i[1] = ft_srchln(i[1], &str, line, 0);
 		i[2] = (str[i[1]] == '\n') ? 0 : 1;
 	}
 	ft_srchln(i[1], &str, line, 1);
-	return ((i[0] == 0) ? 0 : 1);
+	return ((i[2] == 0) ? 0 : 1);
 }
