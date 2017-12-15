24c0e757c98f00ad1be507675c926804224e8368
Gabriele FICARA <gficara@e1r2p12.42.fr>
2017-12-15 14:07:19 +0100

    functioning get nextline (1 fd, not norm proper):w

--- get_next_line.c
+++ get_next_line.c
@@ -5,7 +5,7 @@
 
 #define BUFFER_SIZE 30
 
-static int		ft_srchln(int j, char **str, char **line, int use)
+static int	ft_srchln(int j, char **str, char **line, int use)
 {
 	char	*new;
 
@@ -35,18 +35,22 @@ static int		ft_srchln(int j, char **str, char **line, int use)
 **i[0] = buf, i[1] = string to cpy, i[2] = while flag
 */
 
-
-int		get_next_line(const int fd, char **line)
+int			get_next_line(const int fd, char **line)
 {
 	char	buf[BUFFER_SIZE + 1];
 	static char	*str;
 	int		i[3];
 
 	i[0] = 1;
+	i[1] = 0;
 	i[2] = 1;
-	i[1] = (!str) ? 0 : ft_srchln(i[1], &str, line, 0);
-	str = (i[1] == 0) ? ft_strnew(0) : str;
-	i[2] = (i[1] == 0) ? 1 : 0;
+	if (str)
+	{
+		i[1] = ft_srchln(i[1], &str, line, 0);
+		i[2] = (str[i[1]] == '\n') ? 0 : 1;
+	}
+	else
+		str = ft_strnew(0);
 	while (i[0] && i[2] == 1)
 	{
 		i[0] = read(fd, buf, BUFFER_SIZE);
@@ -55,12 +59,8 @@ int		get_next_line(const int fd, char **line)
 		buf[i[0]] = '\0';
 		str = ft_sfstrjoin(str, buf, 1);
 		i[1] = ft_srchln(i[1], &str, line, 0);
-//		ft_putendl(str);
-//		ft_putnbrendl(i[1]);
 		i[2] = (str[i[1]] == '\n') ? 0 : 1;
-//		ft_putnbrendl(i[2]);
 	}
 	ft_srchln(i[1], &str, line, 1);
-//	ft_putendl(str);
 	return ((i[0] == 0) ? 0 : 1);
 }
